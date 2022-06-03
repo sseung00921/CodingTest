1a2bd7d9aaa3cd9e262ac783416d62308c81754d
이상승 <lss9209@github.com>
2022-06-03 11:58:16 +0900

    알고리즘 문제해결 전략 세트 Treap 구현 완료.

--- libraryForCodingTest/Treap.py
+++ libraryForCodingTest/Treap.py
@@ -1,29 +1,25 @@
-#왠지 모르겠는데 이거 kth랑 erase 함수가 잘 동작 안함. 아무튼 혹시 나중에 수정하거나 참고할 일이 있을 것 같아 남겨둠.
-
-import random
+import random;
 
 class Node :
-    def __init__(self, key, totalNodeCnt) :
+    def __init__(self, key):
         self.key = key;
-        self.priority = random.randint(1, totalNodeCnt)
+        self.priority = random.randint(1, 50000);
         self.size = 1;
         self.left = None;
         self.right = None;
 
-    def setLeft(self, newLeftNode) :
-        self.left = newLeftNode;
+    def calcSize(self):
+        self.size = 1;
+        if self.left != None : self.size += self.left.size;
+        if self.right != None : self.size += self.right.size;
+
+    def setLeft(self, newLeft):
+        self.left = newLeft;
         self.calcSize();
 
-    def setRight(self, newRightNode) :
-        self.right = newRightNode;
+    def setRight(self, newRight):
+        self.right = newRight;
         self.calcSize();
-
-    def calcSize(self) :
-        size = 1;
-        if self.left != None :
-            size += self.left.size;
-        if self.right != None :
-            size += self.right.size;
 
 def split(root, key) :
     if root == None :
@@ -36,32 +32,40 @@ def split(root, key) :
     root.setLeft(ls[1]);
     return (ls[0], root);
 
-def insert(root, addedNode) :
+def insert(root, node) :
     if root == None :
-        return addedNode;
-    if root.priority < addedNode.priority :
-        splitted = split(root, addedNode.key);
-        addedNode.setLeft(splitted[0]);
-        addedNode.setRight(splitted[1]);
-        return addedNode;
-    elif addedNode.key < root.key :
-        root.setLeft(insert(root.left, addedNode));
+        return node;
+    if root.priority < node.priority :
+        splitted = split(root, node.key);
+        node.setLeft(splitted[0]);
+        node.setRight(splitted[1]);
+        return node;
+    elif node.key < root.key :
+        root.setLeft(insert(root.left, node));
     else :
-        root.setRight(insert(root.right, addedNode));
+        root.setRight(insert(root.right, node));
     return root;
 
 def merge(a, b) :
     if a == None : return b;
     if b == None : return a;
     if a.priority < b.priority :
-        b.setLeft(merge(a, b.left));
-        return b;
-    a.setRight(merge(a.right, b));
-    return a;
+        if a.key < b.key :
+            b.setLeft(merge(a, b.left));
+            return b;
+        else :
+            b.setRight(merge(a, b.right));
+            return b;
+    else :
+        if a.key < b.key :
+            a.setRight(merge(a.right, b));
+            return a;
+        else :
+            a.setLeft(merge(a.left, b));
+            return a;
 
 def erase(root, key) :
-    if root == None :
-        return root
+    if root == None : return root;
     if root.key == key :
         ret = merge(root.left, root.right);
         del root;
@@ -74,22 +78,17 @@ def erase(root, key) :
 
 def kth(root, k) :
     leftSize = 0;
-    if root.left != None :
-        leftSize = root.left.size;
-    if k <= leftSize :
-        return kth(root.left, k);
-    if k == leftSize + 1 :
-        return root;
+    if root.left != None : leftSize = root.left.size;
+    if k <= leftSize : return kth(root.left, k);
+    if k == leftSize + 1 : return root;
     return kth(root.right, k - leftSize - 1);
 
-def countLessThan(root, key) :
-    if root == None :
-        return 0;
+def countLessThan(root, key) : #이 함수가 곧 특정 키의 인덱스를 반환해주는 함수와 같다. 즉 위의 함수와 인풋 아웃풋이 반대인 함수이다.
+    if root == None : return 0;
     if root.key >= key :
         return countLessThan(root.left, key);
     ls = root.left.size if root.left != None else 0;
     return ls + 1 + countLessThan(root.right, key);
-
 
 def inOrderTraverse(root) :
     if root == None :
@@ -98,12 +97,15 @@ def inOrderTraverse(root) :
     print(root.key, end = ' ');
     inOrderTraverse(root.right);
 
-nodeArr = [Node(i + 1, 50000) for i in range(0, 5)];
-root = nodeArr[0];
-for i in range(len(nodeArr)) :
-    if i == 0 :
-        continue;
-    root = insert(root, nodeArr[i]);
-print(root.key);
-erase(root, 3);
-inOrderTraverse(root)
+root = None;
+root = insert(root, Node(5));
+root = insert(root, Node(4));
+root = insert(root, Node(3));
+root = insert(root, Node(2));
+root = insert(root, Node(1));
+print(kth(root, 3).key);
+print(countLessThan(root, 3) + 1);
+root = erase(root, 3);
+root = erase(root, 2);
+root = erase(root, 2);
+inOrderTraverse(root);
