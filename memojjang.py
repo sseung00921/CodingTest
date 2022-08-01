260bb433208ece180f03411187f93307719a8669
이상승\lss92 <sangseung@tpmn.io>
2022-08-01 18:15:13 +0900

    백준 푸쉬 102(1874)번

--- memojjang.py
+++ memojjang.py
@@ -1,21 +1,23 @@
+from collections import deque;
+
 n = int(input());
-data = list(map(int, input().split()));
-data.sort();
-m = int(input());
-toFindArr = list(map(int, input().split()));
+wannaBe = [];
+for _ in range(n) :
+    wannaBe.append(int(input()));
 
-def bSearch(left, right, target) :
-    while left <= right :
-        mid = (left + right) // 2
-        if data[mid] == target :
-            return 1;
-        elif data[mid] < target:
-            left = mid + 1
-        elif data[mid] > target:
-            right = mid - 1
-    return 0;
+idx = 0;
+stack = deque([]);
+ops = [];
+for i in range(1, n + 1) :
+    stack.append(i);
+    ops.append('+')
+    while len(stack) > 0  and stack[-1] == wannaBe[idx] :
+        stack.pop();
+        ops.append("-");
+        idx += 1;
 
-
-
-for num in toFindArr :
-    print(bSearch(0, n - 1, num));
+if len(stack) == 0 :
+    for op in ops :
+        print(op);
+else :
+    print("NO");
