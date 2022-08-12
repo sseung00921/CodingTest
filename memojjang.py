ed99f491fab25aba31d133091a8087478eca3daa
이상승\lss92 <sangseung@tpmn.io>
2022-08-12 16:15:07 +0900

    백준 푸쉬 137(1037)번

--- memojjang.py
+++ memojjang.py
@@ -1,22 +1,6 @@
-mapper = {};
-
 n = int(input());
 data = list(map(int, input().split()));
+a = min(data);
+b = max(data);
+print(a*b);
 
-m = int(input());
-toCheckList = list(map(int, input().split()));
-
-for num in data :
-    if num in mapper :
-        mapper[num] += 1;
-    else :
-        mapper[num] = 1;
-
-answerArr = [];
-for toCheck in toCheckList :
-    if toCheck in mapper :
-        answerArr.append(mapper[toCheck]);
-    else :
-        answerArr.append(0);
-
-print(*answerArr);
