eb00714c8711e9ff459bd1146bc70a4d9e6e43a1
이상승\lss92 <sangseung@tpmn.io>
2022-10-06 10:06:38 +0900

    백준 푸쉬 202번

--- memojjang.py
+++ memojjang.py
@@ -1,19 +1,25 @@
 import sys;
 input = sys.stdin.readline;
-n, m = map(int, input().split())
+n = int(input());
+data = list(map(int, input().split()));
 
-def twoCounter(num) :
-    two = 0;
-    while num != 0 :
-        num //= 2;
-        two += num;
-    return two;
+data.sort();
 
-def fiveCounter(num) :
-    five = 0;
-    while num != 0 :
-        num //= 5;
-        five += num;
-    return five;
+Minn = int(1e9*3);
+answer = [];
 
-print(min(twoCounter(n) - twoCounter(n - m) - twoCounter(m), fiveCounter(n) - fiveCounter(n - m) - fiveCounter(m)));
+i = 0;
+j = n - 1;
+while i < j :
+    summ = data[i] + data[j];
+    if abs(summ) < Minn :
+        Minn = abs(summ);
+        answer = [data[i], data[j]];
+        if summ == 0 :
+            break;
+    if summ < 0 :
+        i += 1;
+    elif summ > 0 :
+        j -= 1;
+
+print(*answer);
