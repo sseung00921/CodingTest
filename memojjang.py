4b227e19eda523acd8a75fcd21d6a10869e54329
이상승 <lss9209@github.com>
2022-10-11 19:16:49 +0900

    백준 300제 ~223번

--- memojjang.py
+++ memojjang.py
@@ -1,23 +1,22 @@
 import sys;
 input = sys.stdin.readline;
+n, m = map(int,input().split());
+memoryInfo = [0] + list(map(int, input().split()));
+costInfo = [0] + list(map(int, input().split()));
+SUM = sum(costInfo);
+d = [[0] * (SUM + 1) for _ in range(n + 1)];
 
-n, m = map(int, input().split());
-A = [];
-for _ in range(n) :
-    A.append(list(map(int, input().split())));
-m, k = map(int, input().split());
-B = [];
-for _ in range(m) :
-    B.append(list(map(int, input().split())));
+answer = int(1e9);
+for i in range(1, n + 1) :
+    byte = memoryInfo[i];
+    cost = costInfo[i];
+    for j in range(0, SUM + 1) :
+        if cost > j :
+            d[i][j] = d[i - 1][j];
+        else :
+            d[i][j] = max(byte + d[i - 1][j - cost], d[i - 1][j]);
 
-AB = [[0] * k for _ in range(n)];
+        if d[i][j] >= m :
+            answer = min(answer, j);
 
-for i in range(n) :
-    for j in range(k) :
-        e = 0;
-        for l in range(m) :
-            e += A[i][l]*B[l][j];
-        AB[i][j] = e;
-
-for i in range(len(AB)) :
-    print(*AB[i]);
+print(answer);
