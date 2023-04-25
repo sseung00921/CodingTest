d5953a3401aedf26a0ad09b9e9bf5e52533517b3
이상승 <lss9209@github.com>
2023-04-25 16:25:47 +0900

    올림피아드 풀이 중

--- 2021 KAKAO INTERNSHIP/miro_talchul.py
+++ 2021 KAKAO INTERNSHIP/miro_talchul.py
@@ -62,3 +62,67 @@ end = 3;
 roads = [[1, 2, 2], [3, 2, 3]];
 traps = [2];
 print(solution(n, start, end, roads, traps));
+
+"""
+또 다른 풀이
+import heapq;
+
+Graph = None;
+INF = int(1e15);
+Traps = None;
+N = 0;
+d = None;
+def tuneGraphForThisTurn(trapsStatus) :
+    for i in range((len(bin(trapsStatus)[2 : ]))) :
+        if trapsStatus & (1 << i) > 0 :
+            switchedNode = Traps[i];
+            for j in range(1, N + 1) :
+                if j == switchedNode :
+                    continue;
+                Graph[switchedNode][j], Graph[j][switchedNode] = Graph[j][switchedNode], Graph[switchedNode][j];
+    return;
+
+def solution(n, start, end, roads, traps):
+    global Graph;
+    global Traps;
+    global N;
+    global d;
+    Graph = [[INF] * (n + 1) for _ in range(n + 1)];
+    Traps = traps;
+    N = n;
+    for road in roads :
+        frm, dest, cost = road;
+        Graph[frm][dest] = min(cost, Graph[frm][dest]);
+    d = [[INF] * (1 << len(traps) + 1) for _ in range(N + 1)];
+
+    d[start][0] = 0;
+    q = [(0, start, 0)];
+
+    answer = 0;
+    while q :
+        dist, now, trapsStatus = heapq.heappop(q);
+        if dist > d[now][trapsStatus] :
+            continue;
+        if now == end :
+            answer = dist;
+            break;
+        tuneGraphForThisTurn(trapsStatus);
+        for next in range(1, len(Graph[now])) :
+            if now == next :
+                continue;
+            nDist = d[now][trapsStatus] + Graph[now][next];
+            if next in Traps :
+                idx = Traps.index(next);
+                if trapsStatus & (1 << idx) > 0 :
+                    nTrapStatus = trapsStatus & ~(1 << idx);
+                else :
+                    nTrapStatus = trapsStatus | (1 << idx);
+            else :
+                nTrapStatus = trapsStatus;
+            if nDist < d[next][nTrapStatus] :
+                d[next][nTrapStatus] = nDist;
+                heapq.heappush(q, (nDist, next, nTrapStatus));
+        tuneGraphForThisTurn(trapsStatus);
+        
+    return answer
+"""
