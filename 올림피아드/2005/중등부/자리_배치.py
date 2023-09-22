08a1388dedc19c4132ac98089f6f9c27a80fb428
sangseunglee <sangseunglee.tmax@gmail.com>
2023-09-22 17:21:30 +0900

    올림피아드 풀이

--- 올림피아드/2005/중등부/자리_배치.py
+++ 올림피아드/2005/중등부/자리_배치.py
@@ -1,9 +1,12 @@
 import sys;
 
 input = sys.stdin.readline;
+N = int(input());
+K = int(input());
 dFull = [0] * 41;
 dOneEmpty = [0] * 41;
 
+dFull[0] = 1;
 dFull[1] = 1;
 dFull[2] = 2;
 for i in range(3, 41) :
@@ -12,3 +15,11 @@ for i in range(3, 41) :
 dOneEmpty[2] = 2;
 for i in range(3, 41) :
     dOneEmpty[i] = dOneEmpty[i - 1] + dFull[i - 1];
+
+Answer = 0;
+#빈자리 안 씀
+Answer += dFull[K - 1] * dFull[N - K];
+#빈자리 씀
+for i in range(1, N + 1) :
+    if i == K :
+        continue;
