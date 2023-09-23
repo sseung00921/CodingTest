759c6e07f76b680f56b5f23dda4c369881f3f228
Sangseung Lee <lss9209@naver.com>
2023-09-23 11:10:01 +0900

    올림피아드 풀이 중

--- 올림피아드/2005/중등부/자리_배치.py
+++ 올림피아드/2005/중등부/자리_배치.py
@@ -12,6 +12,8 @@ dFull[2] = 2;
 for i in range(3, 41) :
     dFull[i] = dFull[i - 1] + dFull[i - 2];
 
+dOneEmpty[0] = 1;
+dOneEmpty[1] = 1;
 dOneEmpty[2] = 2;
 for i in range(3, 41) :
     dOneEmpty[i] = dOneEmpty[i - 1] + dFull[i - 1];
@@ -19,12 +21,20 @@ for i in range(3, 41) :
 Answer = 0;
 #빈자리 안 씀
 Answer += dFull[K - 1] * dFull[N - K];
-#빈자리 씀
-for i in range(1, N + 1) :
-    if i == K :
-        continue;
-    if i == 1 :
-        Answer += dOneEmpty[K - 1] * dFull[N - K];
-    if i == N :
-        Answer += dFull[K - 1] * dOneEmpty[N - K];
 
+#빈자리 씀
+for i in range(1, K) :
+    if i == 1 or i == K - 1 :
+        Answer += dOneEmpty[K - 1] * dFull[N - K];
+    else :
+        Answer += (dOneEmpty[i] * dFull[K - i - 1] + dFull[i - 1] * dOneEmpty[K - i]
+                - (dFull[i - 1] * dFull[K - i - 1])) * dFull[N - K];
+
+for i in range(K + 1, N + 1) :
+    if i == K + 1 or i == N :
+        Answer += dFull[K - 1] * dOneEmpty[N - K];
+    else :
+        Answer += dFull[K - 1] * (dOneEmpty[i - K] * dFull[N - i] + dFull[i - K - 1] * dOneEmpty[N - i + 1]
+                - (dFull[i - K - 1] * dFull[N - i]));
+
+print(Answer);
