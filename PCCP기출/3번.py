426a794f0daa904a3520ce6bf39f0c46321a5c59
Sangseung Lee <sseung00921@naver.com>
2023-12-30 12:50:49 +0900

    PCCP 3번 풀이

--- PCCP기출/3번.py
+++ PCCP기출/3번.py
@@ -1,59 +1,49 @@
-def getLongTime(hh, mm, ss) :
+def getNumTime(hh, mm, ss) :
     return int(hh)*3600 + int(mm)*60 + int(ss);
 
-def getStrTime(longTime) :
-    hh = longTime // 3600;
-    longTime -= hh*3600;
-    mm = longTime // 60;
-    ss = longTime % 60;
+def getStrTime(numTime) :
+    hh = numTime // 3600;
+    numTime -= hh*3600;
+    mm, ss = numTime // 60, numTime % 60;
     return hh, mm, ss;
 
 def getHourPos(hh, mm, ss) :
     if hh >= 12 :
         hh -= 12;
-    rtnVal = hh * 5 + mm * (1/12) + ss * (1/720);
-    return rtnVal;
+    return hh*5 + mm*(1/12) + ss*(1/720);
 
 def getMiniPos(mm, ss) :
-    rtnVal = mm + ss * (1/60);
-    return rtnVal;
+    return mm + ss*(1/60);
 
-def calc(startTime, endTime) :
-    rtnCnt = 0;
-    for time in range(startTime, endTime + 1) :
-        if time == 0 or time == 43200 :
-            rtnCnt += 1;
-            continue;
+def solution(h1, m1, s1, h2, m2, s2):
+    startTime = getNumTime(h1, m1, s1);
+    endTIme = getNumTime(h2, m2, s2);
+    answer = 0;
+    for time in range(startTime, endTIme + 1) :
         nowHour, nowMini, nowSec = getStrTime(time);
         nextHour, nextMini, nextSec = getStrTime(time + 1);
-        nowHourPos = getHourPos(nowHour, nowMini, nowSec);
-        nowMiniPos = getMiniPos(nowMini, nowSec);
-        nowSecPos = nowSec;
+        nowHourPos, nowMiniPos, nowSecPos = getHourPos(nowHour, nowMini, nowSec), getMiniPos(nowMini, nowSec), nowSec;
+        nextHourPos, nextMiniPos, nextSecPos = getHourPos(nextHour, nextMini, nextSec), getMiniPos(nextMini, nextSec), nextSec;
 
-        nextHourPos = getHourPos(nextHour, nextMini, nextSec);
-        nextMiniPos = getMiniPos(nextMini, nextSec);
-        nextSecPos = nextSec;
         if nextSecPos == 0 :
             nextSecPos = 60;
         if nextMiniPos == 0 :
             nextMiniPos = 60;
         if nextHourPos == 0 :
             nextHourPos = 60;
-        if nextSecPos == 60 and nextMiniPos == 60 and nextHourPos == 60 :
-            continue;
-        if nowMiniPos == nowSecPos :
-            rtnCnt += 1;
-        elif time != endTime and nowMiniPos > nowSecPos and nextMiniPos < nextSecPos :
-            rtnCnt += 1;
+
         if nowHourPos == nowSecPos :
-            rtnCnt += 1;
-        elif time != endTime and nowHourPos > nowSecPos and nextHourPos < nextSecPos :
-            rtnCnt += 1;
-    return rtnCnt;
+            answer += 1;
+        elif time != endTIme and nowHourPos > nowSecPos and nextHourPos < nextSecPos :
+            answer += 1;
+        if nowMiniPos == nowSecPos :
+            answer += 1;
+        elif time != endTIme and nowMiniPos > nowSecPos and nextMiniPos < nextSecPos :
+            answer += 1;
 
-def solution(h1, m1, s1, h2, m2, s2):
-    startTime = getLongTime(h1, m1, s1);
-    endTime = getLongTime(h2, m2, s2);
-    return calc(startTime, endTime);
+    if startTime <= 0 <= endTIme :
+        answer -= 1;
+    if startTime <= 43200 <= endTIme :
+        answer -= 1;
 
-print(solution(2, 59, 0, 3, 1, 0));
+    return answer;
